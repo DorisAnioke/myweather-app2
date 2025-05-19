8e3d0d5d470427fc37249ce731895131dce29404
Doris Anioke <dorisanioke@gmail.com>
2025-05-18 22:22:05 -0700

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../navigation/Nav.css'
+import { RxFontItalic } from 'react-icons/rx'
+
+const Home = () => {
+  return (
+    <div className='page-content'>
+      <h1>Welcome to my Weather App</h1>
+    </div>
+  )
+}
+
+export default Home
